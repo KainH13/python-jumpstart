bd24ddf06e3bb852cab3518b1edabfd57a834c24
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2019-10-18 22:40:25 -0700

    Added first app

---
+++ hello_world/program.py
@@ -0,0 +1,9 @@
+# Environment testing app, woohoo! WSL works!
+print('--------------------------------------------------')
+print('                    HELLO APP')
+print('--------------------------------------------------')
+print()
+
+user_text = input('what is your name? ')
+greeting = 'Nice to meet you' + ' ' + user_text
+print(greeting)
