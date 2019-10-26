52cb6b95c8b289cb7fb7c2d1721671eeaf0d6520
KainH13 <kneuholdhuber@gmail.com>
2019-10-26 15:36:52 -0700

    Adding compeleted birthday_countdown program

--- birthday_countdown/program.py
+++ birthday_countdown/program.py
@@ -17,13 +17,20 @@ def get_birthday_from_user():
     return birthday
 
 
-def compute_days_between_dates(date1, date2):
-    dt = date1 - date2
+def compute_days_between_dates(original_date, target_date):
+    this_year = datetime.date(target_date.year, original_date.month, original_date.day)
+
+    dt = this_year - target_date
     return dt.days
 
 
-def print_birthday_information():
-    pass
+def print_birthday_information(days):
+    if days < 0:
+        print("You had your birthday {} days ago this year.".format(days))
+    elif days > 0:
+        print("Your birthday is in {} days!".format(days))
+    else:
+        print("Happy birthday!!!")
 
 
 def main():
@@ -32,7 +39,7 @@ def main():
     today = datetime.date.today()
     number_of_days = compute_days_between_dates(bday, today)
     print(number_of_days)
-    # print_birthday_information()
+    print_birthday_information(number_of_days)
 
 
 main()
